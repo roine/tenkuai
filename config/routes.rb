755842d4c7114274ea25718be83106fce73805e8
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-16 18:03:06 +0800

    v0.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Tenkuai::Application.routes.draw do
+  resources :categories
+
+  resources :shirs
+
+  resources :users
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ Tenkuai::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
