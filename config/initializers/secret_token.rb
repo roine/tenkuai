67c3b2a2da4dfc8db3ab7c7589869eb23d51a94c
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-16 15:11:31 +0800

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tenkuai::Application.config.secret_token = 'ff69189ba62fa40dc0183a31371b024fdb08e729783f4f5aa77bbb8571c7b92351cd4b047b867d5e0235abef177a52c6b773e31968868a1c6e41867315836410'
