2fb398d50fd7eb38d6e9fd5e20b9f2b88748f102
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-21 15:59:07 +0800

    added missing files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111220103047) do
+ActiveRecord::Schema.define(:version => 20111221075746) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(:version => 20111220103047) do
     t.integer  "order_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "cached_slug"
   end
 
 end
