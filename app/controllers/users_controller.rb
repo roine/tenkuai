455e565e175f2f63e2812e97b70ab763edd169ca
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-22 16:40:13 +0800

    change the editing mode from pass required to pass no required

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,14 +30,14 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @user }
-    end
-  end
-
+      @user = User.new
+   
+       respond_to do |format|
+         format.html # new.html.erb
+         format.json { render :json => @user }
+       end
+     end
+  
   # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
@@ -46,18 +46,18 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, :notice => 'User was successfully created.' }
-        format.json { render :json => @user, :status => :created, :location => @user }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @user.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+            @user = User.new(params[:user])
+        
+            respond_to do |format|
+              if @user.save
+                format.html { redirect_to @user, :notice => 'User was successfully created.' }
+                format.json { render :json => @user, :status => :created, :location => @user }
+              else
+                format.html { render :action => "new" }
+                format.json { render :json => @user.errors, :status => :unprocessable_entity }
+              end
+            end
+          end
 
   # PUT /users/1
   # PUT /users/1.json
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ActiveRecord::Base
   has_many :shirs
   
   validates :username, :presence=>true,:uniqueness=>true
+  def update_with_password(params={})
+    params.delete(:current_password)
+    self.update_without_password(params)
+  end
  end
