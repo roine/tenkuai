ca78a6b096c129b2ae194e24166bf1b5ebf8b5c6
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-23 12:28:53 +0800

    moved the users/new, no bug found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # GET /users
   # GET /users.json
-  before_filter :authenticate_user!, :except => [:show, :index]
+  before_filter :authenticate_user!, :except => [:show]
   
   def index
     @users = User.all
@@ -16,7 +16,9 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    
     @shirs = @user.shirs
+   
     respond_to do |format|
       format.html # show.html.erb
       format.json { render :json => @user }
@@ -25,39 +27,39 @@ class UsersController < ApplicationController
 
   # GET /users/new
   # GET /users/new.json
-  def new
+      # def new
+      #     #redirect_to :controller=> 'registrations', :action=> 'new'
+      #       @user = User.new
+      #    
+      #        respond_to do |format|
+      #          format.html # new.html.erb
+      #          format.json { render :json => @user }
+      #        end
+      #      end
+      #   
+      # GET /users/1/edit
+      def edit
+        redirect_to "/users/edit"
+        @user = User.find(params[:id])
+      end
     
-      @user = User.new
-   
-       respond_to do |format|
-         format.html # new.html.erb
-         format.json { render :json => @user }
-       end
-     end
-  
-  # GET /users/1/edit
-  def edit
-    redirect_to "/users/edit"
-    @user = User.find(params[:id])
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    
-            @user = User.new(params[:user])
-        
-            respond_to do |format|
-              if @user.save
-                format.html { redirect_to @user, :notice => 'User was successfully created.' }
-                format.json { render :json => @user, :status => :created, :location => @user }
-              else
-                format.html { render :action => "new" }
-                format.json { render :json => @user.errors, :status => :unprocessable_entity }
-              end
-            end
-          end
-
+      # POST /users
+      # POST /users.json
+      # def create
+      #        
+      #                @user = User.new(params[:user])
+      #            
+      #                respond_to do |format|
+      #                  if @user.save
+      #                    format.html { redirect_to @user, :notice => 'User was successfully created.' }
+      #                    format.json { render :json => @user, :status => :created, :location => @user }
+      #                  else
+      #                    format.html { render :action => "new" }
+      #                    format.json { render :json => @user.errors, :status => :unprocessable_entity }
+      #                  end
+      #                end
+      #              end
+      #    
   # PUT /users/1
   # PUT /users/1.json
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Tenkuai::Application.routes.draw do
   devise_for :users, :path_names => { :sign_up => "join", :sign_in => "login", :edit => "pa" }
   resources :categories
-
   resources :shirs
-
-  resources :users
 
   get "home/index"
   
@@ -59,8 +56,8 @@ Tenkuai::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :to => 'home#index'
-   resources :users, :expect => [:show, :index, :new, :create, :edit]
-   resources :users, :only => [:index, :new, :create, :edit]
+   resources :users, :only=>[:show, :index]
+
    match ':id', :as => :username,
                 :via => :get,
                 :controller=> :users,
