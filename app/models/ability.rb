0f2030dcd93c8f7a37c48ee29517df6514a661b9
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-25 12:48:00 +0800

    acl technicaly done v1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,14 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
       user ||= User.new # guest user (not logged in)
-                if user.role?  :admin
+      user.roles.each do |c|
+                if c.name == 'admin'
                   can :manage, :all
-              end
+              else
                     can :read, :all
-             
+             end
+           end
+            
     #
     # The first argument to `can` is the action you are giving the user permission to do.
     # If you pass :manage it will apply to every action. Other common actions here are
