757575316c2ab3c4c22d85b00bdd6933053db6b2
Ling <yalingfu@gmail.com>
2011-12-28 12:22:01 +0800

    devise fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   def index
     @last = User.last
     unless @last.nil?
-
+    
     @created_at = @last.created_at.+8.hours
     @categories = Category.all
+    
+    end
   end
 end
