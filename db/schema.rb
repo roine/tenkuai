11bb50421437a8ed4b10bed93c1ddc324bdc0c4b
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-28 18:24:20 +0800

    option debug in edit users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111224074903) do
+ActiveRecord::Schema.define(:version => 20111228101749) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(:version => 20111224074903) do
   add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
   add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
 
+  create_table "user_types", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "password"
@@ -70,8 +77,8 @@ ActiveRecord::Schema.define(:version => 20111224074903) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "cached_slug"
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",   :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -80,6 +87,7 @@ ActiveRecord::Schema.define(:version => 20111224074903) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "debug",                                 :default => true
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
