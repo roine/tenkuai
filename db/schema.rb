7bfeb1aa7eb4dd7d892ca2843fe3a7882136d7fd
Ling <yalingfu@gmail.com>
2011-12-30 15:07:49 +0800

    categories listing

--- db/schema.rb
+++ db/schema.rb
@@ -82,13 +82,6 @@ ActiveRecord::Schema.define(:version => 20111230033434) do
     t.string "name"
   end
 
-  create_table "user_types", :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "photo"
