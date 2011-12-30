74ba27b181d65bdd307d734b54694ab11d8dd55f
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-30 19:52:36 +0800

    changed the pluralize on profile and dont display the arrows when strictly less than 3 shirs

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   has_many :shirs
   has_and_belongs_to_many :roles
   
-  # before_save :setup_role
+  before_save :setup_role
 
   #allow to save the edit form without typing password
   def update_with_password(params={})
@@ -32,11 +32,11 @@ class User < ActiveRecord::Base
         :small  => "150x150!" }
 
   #setup default role
-  # def setup_role 
-  #     if self.role_ids.empty?     
-  #       self.role_ids = [3] 
-  #     end
-  #   end
+  def setup_role 
+       if self.role_ids.empty?     
+         self.role_ids = [3] 
+       end
+     end
 
 
   #check the username field
