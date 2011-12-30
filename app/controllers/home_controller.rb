0e94448f95e0b472a5621899c1296243d9a471cd
Jonathan de Montalembert <feunetre@hotmail.com>
2011-12-30 21:45:39 +0800

    added title

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   def index
     @last = User.last
     unless @last.nil?
-    
+    @users = User.all
+    @shirs_all = Shir.all
     @created_at = @last.created_at.+8.hours
     @categories = Category.all
     @shirs = Shir.order('created_at desc').limit(6)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(page_title)
+  if page_title.nil?
+    content_for(:title) {"Gig for 20 Kuai"}
+  else
+    content_for(:title) {page_title}
+  end
+  end
 end
