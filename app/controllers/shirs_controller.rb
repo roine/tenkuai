427d7b074d1d1f9fa22f897f6b2c7bebd8cf070d
Jonathan de Montalembert <feunetre@hotmail.com>
2012-01-09 13:38:13 +0800

    clean

--- app/controllers/shirs_controller.rb
+++ app/controllers/shirs_controller.rb
@@ -8,35 +8,6 @@ class ShirsController < ApplicationController
     @categories = Category.all
   end
 
-  def since
-    @shir = Shir.find(params[:id])
-    @since_in_sec = (Time.now.to_i - @shir.created_at.to_i)
-    @since_in_min = @since_in_sec / (60)
-    @since_in_hour = @since_in_min / (60)
-    @since_in_day = @since_in_hour / (24)
-    @since_in_week = @since_in_day / (7)
-    @since_in_month = @since_in_day / (31)
-    @since_in_year = @since_in_day / (365)
-    if @since_in_sec < 60
-      @since = "#{@since_in_sec} second"
-    elsif @since_in_min < 60
-      @since = "#{@since_in_min} minute"
-    elsif @since_in_hour < 24
-      @since = "#{@since_in_hour} hour"
-    elsif @since_in_day < 7
-      @since = "#{@since_in_day} day"
-    elsif @since_in_day < 30
-      @since = "#{@since_in_week} week"
-    elsif @since_in_day < 365
-      @since = "#{@since_in_month} month"
-    elsif @since_in_day >= 365
-      @since = "#{@since_in_year} year"
-    end
-    if @since.to_i > 1
-      @since = @since.pluralize
-    end
-    return @since
-  end
 
   def index
     @shirs = Shir.all
@@ -55,7 +26,6 @@ class ShirsController < ApplicationController
    
     @shir = Shir.find(params[:id])
     @user = @shir.user
-    @since = since
     
     respond_to do |format|
       format.html # show.html.erb
--- friendly_id/ruby/1.8/gems/acts-as-taggable-on-2.2.1/lib/acts-as-taggable-on/version.rb
+++
@@ -1,4 +0,0 @@
-module ActsAsTaggableOn
-  VERSION = '2.2.1'
-end
-
