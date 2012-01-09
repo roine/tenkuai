971eedb087016ce8e387e64925830dc3b64fead8
Jonathan de Montalembert <feunetre@hotmail.com>
2012-01-09 16:54:10 +0800

    added some method in helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,37 @@
 module ApplicationHelper
   def title(page_title)
-  if page_title.nil?
-    content_for(:title) {"Gig for 20 Kuai"}
-  else
-    content_for(:title) {page_title}
+    if !page_title.nil?
+      content_for(:title) {page_title}
+    else
+      content_for(:title) {"Gig for 20 Kuai"}
+    end
   end
-  end
+
   def resource_name
-     :user
-   end
+    :user
+  end
 
-   def resource
-     @resource ||= User.new
-   end
+  def resource
+    @resource ||= User.new
+  end
 
-   def devise_mapping
-     @devise_mapping ||= Devise.mappings[:user]
-   end
-  
-   def city(city, country)
-      unless city.nil?
-        if country.empty?
-          "#{city}"
-        else
-          "#{city},"
-        end
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def city(city, country)
+    unless city.nil?
+      if country.empty?
+        "#{city}"
+      else
+        "#{city},"
       end
     end
+  end
 
-    def country(country)
-      unless country.nil?
-        country
-      end
+  def country(country)
+    unless country.nil?
+      country
     end
+  end
 end
