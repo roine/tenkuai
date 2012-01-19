75a02b61b6f6be1423fbed2900c9b4bfc394f25a
Jonathan de Montalembert <feunetre@hotmail.com>
2012-01-19 23:15:59 +0800

    change the js to have the notification slide

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
-  def pluralize_shir(shirs)
-    return 'Shirs ('+shirs.to_s+')' if shirs > 1
-    return 'Shir'
+  def format_shir
+    shirs = @user.shirs.count
+    return 'Shirs ('<<shirs.to_s<<')' if shirs > 1
+    else 'Shir'
   end
 end
