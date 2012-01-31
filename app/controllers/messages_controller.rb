408c2a22193652cf33c30f31981f3d642d21f0a9
Jonathan de Montalembert <feunetre@hotmail.com>
2012-01-31 10:00:27 +0800

    added pic in the mails

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     redirect_to root_path and return unless own_message?
     @message = current_user.received_messages.find(params[:id])
     # @message.update_attributes(:status => 2)
+    @photo = @message.author.photo_file_name ? @message.author.photo.url(:small) : 'test/santa.jpg'
     user = current_user.id
     corresponding = @message.author.id
     query = MessageCopy.where("sender = #{user}  AND recipient_id =  #{corresponding} OR sender = #{corresponding}  AND recipient_id =  #{user}")
